b133596dfd204395bf3b33d27961e17a7e2e0666
codeIriss <goal.matters.20@gmail.com>
2020-04-14 10:52:19 +0530

    Updates

---
+++ precache-manifest.954310796666d81d75b5a7ae9b3bbc92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89524952d11472e01140e99767da52ad",
+    "url": "/Loaders-react/index.html"
+  },
+  {
+    "revision": "bbd9dc1b4674f8b1c43f",
+    "url": "/Loaders-react/static/css/main.b2abf5ed.chunk.css"
+  },
+  {
+    "revision": "f675c6b7dfcc97d14114",
+    "url": "/Loaders-react/static/js/2.7910660d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Loaders-react/static/js/2.7910660d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbd9dc1b4674f8b1c43f",
+    "url": "/Loaders-react/static/js/main.5a42ae71.chunk.js"
+  },
+  {
+    "revision": "df30f7fd7f1344781c39",
+    "url": "/Loaders-react/static/js/runtime-main.ea515e6c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Loaders-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
